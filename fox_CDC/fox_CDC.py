34f15bc1a4f8def0866169fbab31693277127bf5
foxstock <future.fox.trader@gmail.com>
2024-06-21 14:33:19 +0800

    ...

--- fox_CDC/fox_CDC.py
+++ fox_CDC/fox_CDC.py
@@ -89,67 +89,26 @@ def compare_two_txt_content(txt1,txt2):
         if v!=txt2[i]: return False
     return True # 逐字比對內容無異,視為無變
 
-if __name__ == "__main__":
+def scrapy():
+    global config,last3days,today_str,toHHMMSS_str,FINAL_CSV_DIR,url_dict
     try:
-        #先檢查是否有先前還沒結束的task
-        #將當前視窗名設為"CDC-DAILY"
-        w=WindowMgr()
-        w.set_cmd_title("C-D-C-T-E-M-P")
-        while 1:
-            w.find_window_wildcard("CDC-DAILY")
-            if w.is_exist==True:
-                print("PREV-CDC-DAILY FOUND.KILL IT!")
-                w.set_foreground()
-                w.close_current_window()
-                time.sleep(1)
-            else:
-                break
-            time.sleep(1)
-        #將DOS視窗名設為"MTSL-DAILY"
-        w.find_window_wildcard("C-D-C-T-E-M-P")
-        w.set_cmd_title("CDC-DAILY") 
-        base_dir = os.getcwd()
-        config=ini_to_dict(f"{base_dir}\\setting.ini")
-        #print(config)
-        FINAL_CSV_DIR = base_dir + "\\FINAL\\"
-        # CREATE FINAL CSV DIR IF NOT EXIST
-        create_dir_if_not_exist(FINAL_CSV_DIR)
-        if len(argv)>1:
-            validate_date(argv[1])
-            today_str=argv[1]
-            toHHMMSS_str="233000"
-        else:
-            today_str=strftime("%Y%m%d",time.localtime())
-            toHHMMSS_str=strftime("%H%M%S",time.localtime())
-
-        work_day_str=get_web_content('http://www.web3d.url.tw/workDate/')
-        last3days=get_latest_3days(today_str,work_day_str)
-        #print(last3days)
-        if last3days:
-            print(f"最近三個交易日:{last3days}")
-        else:
-            print(f"{today_str} 不是交易日!")
-            exit()
-
-        url_dict={
-        "證交所首頁":"https://www.twse.com.tw/zh/index.html",
-        "證交所每日當日沖銷交易標的":"https://www.twse.com.tw/zh/trading/day-trading.html",
-        "上櫃現股當沖":"https://www.tpex.org.tw/web/stock/trading/intraday_stat/intraday_trading_stat.php?l=zh-tw"
-        }
-
         service = Service(config['SETTING.chromedriver_loc'])
         # 初始化 Chrome 選項
         options = Options()
         # 添加參數，使 Chrome 瀏覽器最大化
         options.add_argument("--start-maximized")
+        options.add_argument("--disable-notifications")
+        options.add_argument("--disable-gps")
         driver = webdriver.Chrome(service=service, options=options)
         try:
-            if config['SETTING.twse_home_skip']: driver.set_page_load_timeout(3)
+            if config['SETTING.twse_home_skip']: driver.set_page_load_timeout(5)
             driver.get(url_dict["證交所首頁"])
         except:
+            if config['SETTING.twse_home_skip']: driver.set_page_load_timeout(10)
             driver.get(url_dict["證交所每日當日沖銷交易標的"])
+        
+        if config['SETTING.twse_home_skip']: driver.set_page_load_timeout(10)
         final_ls=[]
-    
         for tmp_day in last3days:
             try:
                 # 抓取近三天的上市當沖DATA
@@ -209,8 +168,9 @@ if __name__ == "__main__":
                     # 今日無數據或尚未更新
                     print("證交所每日當日沖銷無數據或尚未更新...")
                     driver.quit()
-                    time.sleep(2)
-                    exit(0)
+                    #time.sleep(2)
+                    #exit(0)
+                    return True
                 else:
                     soup = BeautifulSoup(inner_html, 'html.parser')
 
@@ -234,8 +194,8 @@ if __name__ == "__main__":
             except:
                 #print("上市當沖網頁載入過程出錯，程式終止請重試!")
                 driver.quit()
-                time.sleep(2)
-                exit(0)
+                #exit(0)
+                return False
         time.sleep(5)
         for tmp_day in last3days:
             try:
@@ -262,8 +222,9 @@ if __name__ == "__main__":
                     # 今日無數據或尚未更新
                     print("上櫃當日沖銷無數據或尚未更新...")
                     driver.quit()
-                    time.sleep(2)
-                    exit(0)
+                    #time.sleep(2)
+                    #exit(0)
+                    return True
                 else:
                     tmp_url="https://www.tpex.org.tw/web/stock/trading/intraday_stat/intraday_trading_stat_result.php?l=zh-tw&d="+ac_to_cc(tmp_day)+"&s=0,asc,0&o=htm"
                     driver.get(tmp_url)
@@ -284,10 +245,11 @@ if __name__ == "__main__":
                     ls=re.findall(r"<td[^>]*>(.*?)</td>",str(target_tr))
                     final_ls.append([tmp_day,'400',remove_comma(ls[0]),remove_comma(ls[2]),remove_comma(ls[4])])
             except:
-                print("上櫃當沖網頁載入過程出錯，程式終止請稍候重試...")
+                print("上櫃當沖網頁載入過程出錯，程式終止稍候自動重試...")
                 driver.quit()
-                time.sleep(2)
-                exit(0)
+                #time.sleep(2)
+                #exit(0)
+                return False
         driver.quit()
         #print(final_ls)
         with open(FINAL_CSV_DIR+today_str+toHHMMSS_str+'.csv','w+') as f:
@@ -331,7 +293,60 @@ if __name__ == "__main__":
                             # 若文字檔內容相同則刪除先前已存在的文字檔
                             os.remove(file_path)
                             print(f"文字檔內容相同已刪除:{file_path}")
+        return True
     except Exception as e:
-        print('執行期間發生不可預期之錯誤，強制終止程式...當前自動化Chrome視窗會在下一次採集程式執行時自動關閉。')
+        print('執行期間發生不可預期之錯誤，強制終止程式...10秒後採集程式會重新執行。')
+        driver.quit()
+        return False
+
+if __name__ == "__main__":
+    #先檢查是否有先前還沒結束的task
+    #將當前視窗名設為"CDC-DAILY"
+    w=WindowMgr()
+    w.set_cmd_title("C-D-C-T-E-M-P")
+    while 1:
+        w.find_window_wildcard("CDC-DAILY")
+        if w.is_exist==True:
+            print("PREV-CDC-DAILY FOUND.KILL IT!")
+            w.set_foreground()
+            w.close_current_window()
+            time.sleep(1)
+        else:
+            break
         time.sleep(1)
-        exit(0)
+    #將DOS視窗名設為"MTSL-DAILY"
+    w.find_window_wildcard("C-D-C-T-E-M-P")
+    w.set_cmd_title("CDC-DAILY") 
+    base_dir = os.getcwd()
+    config=ini_to_dict(f"{base_dir}\\setting.ini")
+    #print(config)
+    FINAL_CSV_DIR = base_dir + "\\FINAL\\"
+    # CREATE FINAL CSV DIR IF NOT EXIST
+    create_dir_if_not_exist(FINAL_CSV_DIR)
+    if len(argv)>1:
+        validate_date(argv[1])
+        today_str=argv[1]
+        toHHMMSS_str="233000"
+    else:
+        today_str=strftime("%Y%m%d",time.localtime())
+        toHHMMSS_str=strftime("%H%M%S",time.localtime())
+    work_day_str=get_web_content('http://www.web3d.url.tw/workDate/')
+    last3days=get_latest_3days(today_str,work_day_str)
+    #print(last3days)
+    if last3days:
+        print(f"最近三個交易日:{last3days}")
+    else:
+        print(f"{today_str} 不是交易日!")
+        exit()
+    url_dict={
+    "證交所首頁":"https://www.twse.com.tw/zh/index.html",
+    "證交所每日當日沖銷交易標的":"https://www.twse.com.tw/zh/trading/day-trading.html",
+    "上櫃現股當沖":"https://www.tpex.org.tw/web/stock/trading/intraday_stat/intraday_trading_stat.php?l=zh-tw"
+    }
+    while 1:
+        result=scrapy()
+        #print(f"result:{result}")
+        if result:
+            exit(0)
+        else:
+            time.sleep(10)
--- fox_CLENDX/fox_CLENDX.py
+++ fox_CLENDX/fox_CLENDX.py
@@ -38,46 +38,9 @@ def ac_to_cc(date_str):
 def add_slash_to_date_str(date_str):
     return f"{date_str[0:4]}/{date_str[4:6]}/{date_str[6:8]}"
 
-if __name__ == "__main__":
+def scrapy():
+    global config,today_str,toHHMMSS_str,FINAL_CSV_DIR,url_dict
     try:
-        #先檢查是否有先前還沒結束的task
-        #將當前視窗名設為"CLENDX-DAILY"
-        w=WindowMgr()
-        w.set_cmd_title("C-L-E-N-D-X-T-E-M-P")
-        while 1:
-            w.find_window_wildcard("CLENDX-DAILY")
-            if w.is_exist==True:
-                print("PREV-CLENDX-DAILY FOUND.KILL IT!")
-                w.set_foreground()
-                w.close_current_window()
-                time.sleep(1)
-            else:
-                break
-            time.sleep(1)
-        #將DOS視窗名設為"MTSL-DAILY"
-        w.find_window_wildcard("C-L-E-N-D-X-T-E-M-P")
-        w.set_cmd_title("CLENDX-DAILY") 
-        base_dir = os.getcwd()
-        config=ini_to_dict(f"{base_dir}\\setting.ini")
-        #print(config)
-        FINAL_CSV_DIR = base_dir + "\\FINAL\\"
-        # CREATE FINAL CSV DIR IF NOT EXIST
-        create_dir_if_not_exist(FINAL_CSV_DIR)
-        if len(argv)>1:
-            validate_date(argv[1])
-            today_str=argv[1]
-            toHHMMSS_str="233000"
-        else:
-            today_str=strftime("%Y%m%d",time.localtime())
-            toHHMMSS_str=strftime("%H%M%S",time.localtime())
-
-        yy=today_str[0:4]
-        mm=str(int(today_str[4:6]))
-        dd=str(int(today_str[6:8]))
-        url_dict={
-            "證交所首頁":"https://www.twse.com.tw/zh/index.html",
-            "證交所LENDX":f"https://www.twse.com.tw/rwd/zh/lending/TWT72U?date={today_str}"
-        }
         service = Service(config['SETTING.chromedriver_loc'])
         # 初始化 Chrome 選項
         options = Options()
@@ -85,11 +48,12 @@ if __name__ == "__main__":
         options.add_argument("--start-maximized")
         driver = webdriver.Chrome(service=service, options=options)
         try:
-            if config['SETTING.twse_home_skip']: driver.set_page_load_timeout(3)
+            if config['SETTING.twse_home_skip']: driver.set_page_load_timeout(5)
             driver.get(url_dict["證交所首頁"])
         except:
+            if config['SETTING.twse_home_skip']: driver.set_page_load_timeout(10)
             driver.get(url_dict["證交所LENDX"])
-
+        if config['SETTING.twse_home_skip']: driver.set_page_load_timeout(10)
         driver.get(url_dict["證交所LENDX"])
 
         time.sleep(3)
@@ -99,7 +63,8 @@ if __name__ == "__main__":
         if len(source_code)<500:
             print("LENDX 無數據或尚未更新...")
             driver.quit()
-            exit(0)
+            #exit(0)
+            return True
         #print(source_code)
         json_str=re.findall(r"<pre>(.*?)</pre>",str(source_code))
         #print(json_str)
@@ -123,7 +88,53 @@ if __name__ == "__main__":
                 if size < 2048:
                     os.remove(file_path)
                     print(f'已刪除0KB檔案：{filename}')
+        return True
     except Exception as e:
-        print('執行期間發生不可預期之錯誤，強制終止程式...當前自動化Chrome視窗會在下一次採集程式執行時自動關閉。')
+        print('執行期間發生不可預期之錯誤，強制終止程式...10秒後採集程式會重新執行。')
+        driver.quit()
+        return False
+if __name__ == "__main__":
+    #先檢查是否有先前還沒結束的task
+    #將當前視窗名設為"CLENDX-DAILY"
+    w=WindowMgr()
+    w.set_cmd_title("C-L-E-N-D-X-T-E-M-P")
+    while 1:
+        w.find_window_wildcard("CLENDX-DAILY")
+        if w.is_exist==True:
+            print("PREV-CLENDX-DAILY FOUND.KILL IT!")
+            w.set_foreground()
+            w.close_current_window()
+            time.sleep(1)
+        else:
+            break
         time.sleep(1)
-        exit(0)
+    #將DOS視窗名設為"MTSL-DAILY"
+    w.find_window_wildcard("C-L-E-N-D-X-T-E-M-P")
+    w.set_cmd_title("CLENDX-DAILY") 
+    base_dir = os.getcwd()
+    config=ini_to_dict(f"{base_dir}\\setting.ini")
+    #print(config)
+    FINAL_CSV_DIR = base_dir + "\\FINAL\\"
+    # CREATE FINAL CSV DIR IF NOT EXIST
+    create_dir_if_not_exist(FINAL_CSV_DIR)
+    if len(argv)>1:
+        validate_date(argv[1])
+        today_str=argv[1]
+        toHHMMSS_str="233000"
+    else:
+        today_str=strftime("%Y%m%d",time.localtime())
+        toHHMMSS_str=strftime("%H%M%S",time.localtime())
+    yy=today_str[0:4]
+    mm=str(int(today_str[4:6]))
+    dd=str(int(today_str[6:8]))
+    url_dict={
+        "證交所首頁":"https://www.twse.com.tw/zh/index.html",
+        "證交所LENDX":f"https://www.twse.com.tw/rwd/zh/lending/TWT72U?date={today_str}"
+    }
+    while 1:
+        result=scrapy()
+        #print(f"result:{result}")
+        if result:
+            exit(0)
+        else:
+            time.sleep(10)
